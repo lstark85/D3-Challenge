89a99a5d567b02d7f77e0092287f036b3e9c6958
Linda Stark <lindagiang85@gmail.com>
2021-08-04 21:40:49 -0700

    scatter plot initial

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,27 @@
-// @TODO: YOUR CODE HERE!
+// Create dimensions of chart
+
+// Designate the width of the graph
+var width = parseInt(d3.select("#scatter").style("width"));
+
+// Designate the height of the graph
+var height = width - width / 3.9;
+
+// Margin spacing for graph
+var margin = 20;
+
+// space for placing words
+var labelArea = 110;
+
+// padding for the text at the bottom and left axes
+var tPadBot = 40;
+var tPadLeft = 40;
+
+// create the scatter plot
+var svg = d3.select("scatter")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("svg")
+    .attr("class", "chart"); 
+
+// create round state codes and size them
